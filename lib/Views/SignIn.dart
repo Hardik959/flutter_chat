d27599ee419c79b279ed858c874085fd354c6dfb
Hardik959 <hardiksrivastava959@gmail.com>
2022-09-29 21:37:19 +0530

    Screens done

---
+++ lib/Views/SignIn.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+class SignIn extends StatefulWidget {
+  const SignIn({ Key? key }) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
