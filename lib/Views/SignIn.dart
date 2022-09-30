897c2e30741a07507e2bfce12b38ece28cf09fe5
Hardik959 <hardiksrivastava959@gmail.com>
2022-09-30 19:39:43 +0530

    commit

--- lib/Views/SignIn.dart
+++ lib/Views/SignIn.dart
@@ -2,8 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/widgets/widgets.dart';
+
 class SignIn extends StatefulWidget {
-  const SignIn({ Key? key }) : super(key: key);
+  const SignIn({Key? key}) : super(key: key);
 
   @override
   State<SignIn> createState() => _SignInState();
@@ -14,26 +15,49 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(50),
-        child: appbarmain(context)),
-        body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          child: Column(children: [
-            SizedBox(height: 10,),
-          
-          Image.asset("assets/images/connect.jpg"),
-          SizedBox(height: 30,),
-          Divider(thickness: 3,
-          color: Colors.black54,),
-          
-          TextField(
-            onTap: (() {
-            }),
-        
-          ),
-        ],)),
-      
+          preferredSize: const Size.fromHeight(50), child: appbarmain(context)),
+      body: SingleChildScrollView(
+        child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Image.asset("assets/images/connect.jpg"),
+                SizedBox(
+                  height: 30,
+                ),
+                Divider(
+                  thickness: 1,
+                  color: Colors.black54,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    hintStyle: TextStyle(),
+                  ),
+                  onTap: (() {}),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    
+                    hintText: "Password",
+                    hintStyle: TextStyle(),
+                  ),
+                  onTap: (() {}),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
