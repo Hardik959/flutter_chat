728677a1c9711364b5db6cfaeb801f94bbe0aa2c
Hardik959 <hardiksrivastava959@gmail.com>
2022-09-30 18:39:23 +0530

    commit

--- lib/Views/SignIn.dart
+++ lib/Views/SignIn.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/widgets/widgets.dart';
 class SignIn extends StatefulWidget {
   const SignIn({ Key? key }) : super(key: key);
 
@@ -11,7 +12,27 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: appbarmain(context)),
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(children: [
+            SizedBox(height: 10,),
+          
+          Image.asset("assets/images/connect.jpg"),
+          SizedBox(height: 30,),
+          Divider(thickness: 3,
+          color: Colors.black54,),
+          
+          TextField(
+            onTap: (() {
+            }),
+        
+          ),
+        ],)),
       
     );
   }
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+Widget appbarmain(BuildContext context) {
+  return AppBar(
+    centerTitle: true,
+    backgroundColor: Colors.black87,
+    title: Text("Connect",
+    style: TextStyle(color: Colors.grey),),
+  );
+}
